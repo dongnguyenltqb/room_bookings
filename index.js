069f1447aa43ebb2fb0c93586b165d74d2a5e3a8
dong <dong.nguyen@spiritlabs.co>
2021-06-28 19:18:55 +0700

    add func

--- index.js
+++ index.js
@@ -1,8 +1,90 @@
+const moment = require("moment");
+const { Transaction, QueryTypes } = require("sequelize");
 const models = require("./models");
 models.Instant.authenticate()
-  .then((data) => startProcess())
+  .then(() => startProcess())
   .catch((err) => console.log(err));
 
-async function booking(room_id, start_time, end_time, order_id) {}
+// return error || sucessful
+async function book(room_id, start_time, end_time, order_id) {
+  let t = await models.Instant.transaction({
+    isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED,
+  });
 
-async function startProcess() {}
+  try {
+    // aquire explicit lock
+    await models.Instant.query(
+      `select * from rooms where id = :room_id for update;`,
+      {
+        transaction: t,
+        type: QueryTypes.SELECT,
+        replacements: {
+          room_id,
+        },
+      }
+    );
+    // check overlaptime
+    let total = await models.Instant.query(
+      `
+        select * from bookings
+        where room_id = :room_id
+        and (not (
+        (start_time > :end_time ) or (end_time < :start_time)
+        ))
+        limit 1;
+      `,
+      {
+        transaction: t,
+        type: QueryTypes.SELECT,
+        replacements: {
+          room_id,
+          start_time,
+          end_time,
+        },
+      }
+    );
+    if (total.length > 0) throw new Error(`${room_id} overlaptime`);
+
+    // insert now
+    await models.Bookings.create(
+      {
+        room_id,
+        start_time,
+        end_time,
+        order_id,
+      },
+      {
+        transaction: t,
+      }
+    );
+    await t.commit();
+    return true;
+  } catch (err) {
+    t.rollback();
+    return err.message;
+  }
+}
+
+async function startProcess() {
+  let startTime = moment().unix();
+  let n = 1000;
+  let maxRoomCount = 12;
+  let arrayOfPromises = [];
+  for (let room_id = 1; room_id <= maxRoomCount; room_id++) {
+    for (let i = 0; i < n; i++) {
+      let start_time = moment().unix() + Math.floor(Math.random() * 100000000);
+      let end_time = start_time + Math.floor(Math.random() * 6000000);
+
+      start_time = moment(start_time * 1000).toISOString();
+      end_time = moment(end_time * 1000).toISOString();
+
+      arrayOfPromises.push(book(room_id, start_time, end_time, 1));
+    }
+  }
+
+  await Promise.all(arrayOfPromises);
+
+  console.log(
+    `Process ${n * maxRoomCount} command in ${moment().unix() - startTime} s`
+  );
+}
--- models.js
+++ models.js
@@ -1,54 +1,79 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize(
-  "postgres://postgres:123456@localhost:5432/postgres"
+  "postgres://postgres:123456@localhost:5432/postgres",
+  {
+    logging: false,
+  }
 );
 
-const Bookings = sequelize.define("bookings", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Bookings = sequelize.define(
+  "bookings",
+  {
+    room_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    start_time: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    end_time: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    order_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
-  start_time: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  end_time: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
+Bookings.removeAttribute("id");
 
-const Rooms = sequelize.define("rooms", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Rooms = sequelize.define(
+  "rooms",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
-const Orders = sequelize.define("orders", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Orders = sequelize.define(
+  "orders",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    status: {
+      type: DataTypes.STRING,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    expired_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
   },
-  status: {
-    type: DataTypes.STRING,
-  },
-  created_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  expired_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 module.exports = {
   Rooms,
