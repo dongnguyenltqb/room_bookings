7d4dae82221959f768497f6aae7820420660ad45
dong <dong.nguyen@spiritlabs.co>
2021-06-28 20:02:34 +0700

    multi room bookings

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const moment = require("moment");
 const { Transaction, QueryTypes } = require("sequelize");
 const models = require("./models");
 models.Instant.authenticate()
-  .then(() => startProcess())
+  .then(startProcess)
+  .then(startProcessMulti)
+  .then(() => process.exit(0))
   .catch((err) => console.log(err));
+
+async function removeCommandFromOrder(order_id) {
+  await models.Bookings.destroy({
+    where: {
+      order_id,
+    },
+  });
+}
 
 // return error || sucessful
 async function book(room_id, start_time, end_time, order_id) {
@@ -85,6 +95,55 @@ async function startProcess() {
   await Promise.all(arrayOfPromises);
 
   console.log(
-    `Process ${n * maxRoomCount} command in ${moment().unix() - startTime} s`
+    `Process ${n * maxRoomCount} single command in ${
+      moment().unix() - startTime
+    } s`
+  );
+}
+
+async function startProcessMulti() {
+  let startTime = moment().unix();
+  // n ís the number of order
+  let n = 1000;
+  let maxRoomCount = 12;
+  let arrayOfNItems = [];
+  for (let i = 1; i <= n; i++) arrayOfNItems.push(i);
+
+  // process order
+  let data = await Promise.all(
+    arrayOfNItems.map(async (order_id) => {
+      let commands = new Array(maxRoomCount + 1);
+      // order include option from multi room
+      for (let room_id = 1; room_id <= maxRoomCount; room_id++) {
+        let start_time =
+          moment().unix() + Math.floor(Math.random() * 10000000000);
+        let end_time = start_time + Math.floor(Math.random() * 6000000);
+
+        start_time = moment(start_time * 1000).toISOString();
+        end_time = moment(end_time * 1000).toISOString();
+
+        commands[room_id] = book(room_id, start_time, end_time, order_id);
+      }
+      // wait for process
+      let commandResults = await Promise.all(commands);
+      // check group command for order
+      for (let room_id = 1; room_id <= maxRoomCount; room_id++)
+        if (commandResults[room_id] !== true) {
+          // depend on bussiness
+          await removeCommandFromOrder(order_id);
+          return false;
+        }
+      // all command sucessfully
+      return true;
+    })
+  );
+
+  let count = 0;
+  for (let i = 0; i < data.length; i++) if (data[i]) count = count + 1;
+
+  console.log(
+    `Process ${n * maxRoomCount} multi command in ${
+      moment().unix() - startTime
+    } s, ${count} orders ok`
   );
 }
